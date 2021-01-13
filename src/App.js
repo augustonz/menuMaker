249593541267bda29ee68ac0339a145e364f4eb5
Augusto Nunes Zacarias <augustonz2010@hotmail.com>
2021-01-13 10:59:52 -0300

    Installed vercel and created first screens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import './App.css';
+import { Router,Route,Switch,Link} from 'react-router-dom';
+import CreateMenu from './views/CreateMenu';
+import Menu from './views/Menu';
 
 function App() {
 
   return (
     <>
-      <h1>João Pedro é gay!</h1>
+      <p>test</p>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Menu/>
+          </Route>
+          <Route path='/loja/117/menu'>
+            <CreateMenu/>
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
---
+++ src/views/CreateMenu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class CreateMenu extends React.Component {
+    render() {
+        return(
+            <h1>This is the Create Menu page</h1>
+        )
+    }
+}
+
